52f3aebad69506c3f301425197352673100b2ac4
Luke <lr98lr@aol.com>
2016-11-24 22:52:30 -0500

    cleaned

--- LengineConfig/main.cpp
+++ LengineConfig/main.cpp
@@ -7,13 +7,6 @@
 //start of app
 int main(int argc, char** argv){
 
-	lml::Mat2 mat(1.0,2.0,3.0,4.0);
-	std::cout << "\n\nCurrent matrix:\n[" << mat.elementAt(0, 0) << "  " << mat.elementAt(1, 0) << "]\n[" << mat.elementAt(0, 1) << "  " << mat.elementAt(1, 1);
-	mat.transpose();
-	std::cout << "\n\Transpose matrix:\n[" << mat.elementAt(0, 0) << "  " << mat.elementAt(1, 0) << "]\n[" << mat.elementAt(0, 1) << "  " << mat.elementAt(1, 1);
-	mat.transpose();
-	std::cout << "\n\Transpose back:\n[" << mat.elementAt(0, 0) << "  " << mat.elementAt(1, 0) << "]\n[" << mat.elementAt(0, 1) << "  " << mat.elementAt(1, 1);
-	SDL_Delay(500000);
 	//run the main loop
 	LengineCore core;
 	core.run();
--- lml/Mat3x3.h
+++ lml/Mat3x3.h
@@ -3,6 +3,9 @@ class Mat3x3
 {
 public:
 	Mat3x3();
+
+
+	void transpose();
 	~Mat3x3();
 };
 

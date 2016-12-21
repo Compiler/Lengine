6c89b03d7e6cd5b61f48eb1a55bd4a3c6afbacf2
Luke <lr98lr@aol.com>
2016-12-21 03:03:41 -0500

    color and Vertex structures

--- LengineConfig/Vertex.h
+++ LengineConfig/Vertex.h
@@ -1,15 +1,20 @@
 #pragma once
+#include <GL\glew.h>
+#include <lml\Vector2f.h>
 
-
-struct vec2 {
-	float x;
-	float y;
-	
+struct color {
+	GLfloat r, g, b, a;
+	void set(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha) {
+		r = red;
+		g = green;
+		b = blue;
+		a = alpha;
+	}
 };
 
 
 
 struct Vertex {
-
-
+	color color;
+	lml::Vector2f position;
 };

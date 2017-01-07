f88d87b5d85a45346cdcd9a15ee33f525391cd88
Luke <lr98lr@aol.com>
2017-01-07 10:00:34 -0500

    initializing

--- LengineConfig/main.cpp
+++ LengineConfig/main.cpp
@@ -9,13 +9,13 @@
 //start of app
 int main(int argc, char** argv){
 	
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		std::cout << "Error initializing SDL";
-	}
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) std::cout << "Error initializing SDL\n";
 
-
-
+	SDL_Window *window;
+	window = SDL_CreateWindow("Temporary", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	SDL_GL_CreateContext(window);
+	if(glewInit() != GLEW_OK) std::cout << "Error initializing GLEW\n";
 	
-
+	system("PAUSE");
 	return 0;
 }

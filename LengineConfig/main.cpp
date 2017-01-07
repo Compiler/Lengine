6d308f8a255b8b661bc8ae8dd11a9da482b713e9
Luke <lr98lr@aol.com>
2017-01-07 09:51:53 -0500

    iniit

--- LengineConfig/main.cpp
+++ LengineConfig/main.cpp
@@ -1,12 +1,20 @@
 
 #include <iostream>
 
-
+#include <GL\glew.h>
+#include <SDL\SDL.h>
 
 
 
 //start of app
 int main(int argc, char** argv){
+	
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cout << "Error initializing SDL";
+	}
+
+
+
 	
 
 	return 0;

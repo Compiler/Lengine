0ad475b889e5afd5e0b6c7f02de21c218496d1a7
Luke <lr98lr@aol.com>
2017-01-09 09:03:28 -0500

    added printing method of finding errors in glsl code

--- LengineConfig/ShaderProgram.cpp
+++ LengineConfig/ShaderProgram.cpp
@@ -26,15 +26,52 @@ void ShaderProgram::create(const GLchar *vertexFilePath, const GLchar *fragFileP
 	glShaderSource(_vertexID, 1, &source, nullptr);
 	glShaderSource(_fragmentID, 1, &fragSource, nullptr);
 
+
+
 	glCompileShader(_vertexID);
+	//Now check if vertex shader compiled correctly
+	GLint isCompiled = 0;
+	glGetShaderiv(_vertexID, GL_COMPILE_STATUS, &isCompiled);
+	if (isCompiled == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetShaderiv(_vertexID, GL_INFO_LOG_LENGTH, &maxLength);
+
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(_vertexID, maxLength, &maxLength, &errorLog[0]);
+
+
+		std::cout << "\nError in compiling Vertex Shader:\n";
+		for (int i = 0; i < errorLog.size(); i++) {
+			std::cout << errorLog[i];
+		}
+
+		glDeleteShader(_vertexID);
+		glDeleteShader(_fragmentID);
+		return;
+	}
+
+
 	glCompileShader(_fragmentID);
 
-	GLint compileStatus;
-	glGetShaderiv(_shaderProgramID, GL_COMPILE_STATUS, &compileStatus);
-	if (compileStatus == GL_FALSE) {
-		GLchar message[256];
-		glGetShaderInfoLog(_shaderProgramID, sizeof(message), 0, &message[0]);
-		std::cout << "Couldn't compile shader:\n" << message;
+	//Now check if fragment shader compiled correctly
+	isCompiled = 0;
+	glGetShaderiv(_fragmentID, GL_COMPILE_STATUS, &isCompiled);
+	if (isCompiled == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetShaderiv(_fragmentID, GL_INFO_LOG_LENGTH, &maxLength);
+
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(_fragmentID, maxLength, &maxLength, &errorLog[0]);
+
+		std::cout << "\nError in compiling Fragment Shader:\n";
+		for (int i = 0; i < errorLog.size(); i++) {
+			std::cout << errorLog[i];
+		}
+		glDeleteShader(_vertexID);
+		glDeleteShader(_fragmentID);
+		return;
 	}
 
 

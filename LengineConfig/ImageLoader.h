437c10cd515ec09fef99c62db93313729e1a09e3
Luke <lr98lr@aol.com>
2017-01-14 12:57:40 -0500

    custom bmp loader WORKINGgit add -Agit add -Agit add -A!

--- LengineConfig/ImageLoader.h
+++ LengineConfig/ImageLoader.h
@@ -10,9 +10,9 @@
 #include <vector>
 
 
-void LoadBMP(const char * fileName, std::vector<unsigned char> &buffer, unsigned long &width, unsigned long &height){
+void LoadBMP(const char * fileName, unsigned long &width, unsigned long &height, GLuint &texture){
 
-
+	
 	std::fstream file;
 	file.open(fileName, std::fstream::binary | std::fstream::in);
 
@@ -59,14 +59,28 @@ void LoadBMP(const char * fileName, std::vector<unsigned char> &buffer, unsigned
 	std::cout << "Bits per pixel: " << bpp;
 	std::cout << "\nCompression Method: " << compressionMethod << "\n";
 	//start of pixel array - 50
-	unsigned char *pixels = new unsigned char[length - headerOffset];
-	file.seekg(headerOffset);
-	file.read((char *)pixels, length - headerOffset);
+	unsigned char *pixels = new unsigned char[width*height * 3];
+	file.seekg(headerOffset + 40);
+	file.read((char *)pixels, width*height*3);
 
-	buffer.resize(length - headerOffset + 1);
-	for (int i = 0; i < length - headerOffset; i++)
-		buffer[i] = pixels[i];
-	buffer[length - headerOffset + 1] = '\0';
+	unsigned char tmpRGB = 0; // Swap buffer
+	for (unsigned long i = 0; i < width * height * 3; i += 3)
+	{
+		tmpRGB = pixels[i];
+		pixels[i] = pixels[i + 2];
+		pixels[i + 2] = tmpRGB;
+	}
+
+	glGenTextures(1, &texture);             // Generate a texture
+	glBindTexture(GL_TEXTURE_2D, texture); // Bind that texture temporarily
+
+	GLint mode = GL_RGB;                   // Set the mode
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, mode, width, height, 0, mode, GL_UNSIGNED_BYTE, pixels);
+
 
 	delete[] pixels;
 	delete[] data;
--- LengineConfig/LengineCore.cpp
+++ LengineConfig/LengineCore.cpp
@@ -110,27 +110,8 @@ void LengineCore::init() {
 
 	
 	unsigned long width, height;
-
-	std::vector<unsigned char> buffer, output;
-	std::string info = "Textures/brick.png";
-	IOManager::readFileToBuffer(info, buffer);
-	//int errorCode = decodePNG(output, width, height, &buffer[0], buffer.size());
 	
-	LoadBMP("Textures/wood.bmp", buffer, width, height);
-
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &output[0]);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	glBindTexture(GL_TEXTURE_2D, 0);
+	LoadBMP("Textures/RGB.bmp", width, height, texture);
 }
 
 

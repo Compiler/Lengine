83a5038b7c5f1a48bd274ed73e45c303510cfabf
Luke <lr98lr@aol.com>
2017-01-17 17:08:17 -0500

    implemented operators

--- lml/Vector3.cpp
+++ lml/Vector3.cpp
@@ -7,6 +7,49 @@ Vector3::Vector3(GLfloat x, GLfloat y, GLfloat z): x(x), y(y), z(z){
 }
 
 
+Vector3 Vector3::operator -(const Vector3 &vec) const{
+	return Vector3(x - vec.x, y - vec.y, z - vec.z);
+}
+
+Vector3 Vector3::operator +(const Vector3 &vec) const{
+	return Vector3(x + vec.x, y + vec.y, z + vec.z);
+}
+
+Vector3 Vector3::operator *(float mult) const{
+	return Vector3(x * mult, y * mult, z * mult);
+}
+
+Vector3 Vector3::operator /(float div) const{
+	return Vector3(x / div, y / div, z / div);
+}
+
+
+Vector3 &Vector3::operator -=(const Vector3 &vec){
+	x -= vec.x; y -= vec.y; z -= vec.z;
+	return *this;
+}
+
+Vector3 &Vector3::operator +=(const Vector3 &vec){
+	x += vec.x; y += vec.y; z += vec.z;
+	return *this;
+}
+
+Vector3 &Vector3::operator *=(float mult){
+	x *= mult; y *= mult; z *= mult;
+	return *this;
+}
+
+Vector3 &Vector3::operator /=(float div){
+	x /= div; y /= div; z /= div;
+	return *this;
+}
+
+
+float Vector3::operator *(const Vector3 &vec) const{
+	return (x * vec.x) + (y * vec.y) + (z * vec.z);
+}
+
+
 Vector3::~Vector3(){
 
 

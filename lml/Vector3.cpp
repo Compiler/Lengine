de7b2157539dcc9d8698e4dda3b66cf48700c541
Luke <lr98lr@aol.com>
2017-01-17 17:16:27 -0500

    non member functions

--- lml/Vector3.cpp
+++ lml/Vector3.cpp
@@ -52,9 +52,9 @@ GLfloat Vector3::operator *(const Vector3 &vec) const{
 
 
 void Vector3::normalize(){
-	float magSq = x*x + y*y + z*z;
+	GLfloat magSq = x*x + y*y + z*z;
 	if(magSq > 0.0f){ // check for divide-by-zero
-		float oneOverMag = 1.0f / sqrt(magSq);
+		GLfloat oneOverMag = 1.0f / sqrt(magSq);
 		x *= oneOverMag;
 		y *= oneOverMag;
 		z *= oneOverMag;
@@ -65,3 +65,31 @@ Vector3::~Vector3(){
 
 
 }
+
+
+
+// Compute the magnitude of a vector
+inline float vectorMag(const Vector3 &a){
+	return sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
+}
+// Compute the cross product of two vectors
+inline Vector3 crossProduct(const Vector3 &a, const Vector3 &b){
+	return Vector3(
+		a.y*b.z - a.z*b.y,
+		a.z*b.x - a.x*b.z,
+		a.x*b.y - a.y*b.x
+	);
+}
+// Scalar on the left multiplication, for symmetry
+inline Vector3 operator *(float k, const Vector3 &v){
+	return Vector3(k*v.x, k*v.y, k*v.z);
+}
+// Compute the distance between two points
+inline float distance(const Vector3 &a, const Vector3 &b){
+	float dx = a.x - b.x;
+	float dy = a.y - b.y;
+	float dz = a.z - b.z;
+	return sqrt(dx*dx + dy*dy + dz*dz);
+}
+
+extern const Vector3 kZeroVector;
--- lml/Vector3.h
+++ lml/Vector3.h
@@ -36,6 +36,8 @@ public:
 
 	void normalize();
 
+
+
 	~Vector3();
 };
 

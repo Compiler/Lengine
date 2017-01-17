8b62089d6a26c51cea638758b9ad24567d811e07
Luke <lr98lr@aol.com>
2017-01-17 17:12:21 -0500

    implemented normalization

--- lml/Vector3.cpp
+++ lml/Vector3.cpp
@@ -1,5 +1,5 @@
 #include "Vector3.h"
-
+#include <math.h>
 
 
 Vector3::Vector3(GLfloat x, GLfloat y, GLfloat z): x(x), y(y), z(z){
@@ -50,6 +50,16 @@ GLfloat Vector3::operator *(const Vector3 &vec) const{
 	return (x * vec.x) + (y * vec.y) + (z * vec.z);
 }
 
+
+void Vector3::normalize(){
+	float magSq = x*x + y*y + z*z;
+	if(magSq > 0.0f){ // check for divide-by-zero
+		float oneOverMag = 1.0f / sqrt(magSq);
+		x *= oneOverMag;
+		y *= oneOverMag;
+		z *= oneOverMag;
+	}
+}
 
 Vector3::~Vector3(){
 
